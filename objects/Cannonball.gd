d9804223fb4d9597fde1e3d7068f29c443309633
Nathan <lassinganathan@gmail.com>
2022-03-24 18:46:03 -0700

    Fix cannonball damage logic

--- objects/Cannonball.gd
+++ objects/Cannonball.gd
@@ -5,14 +5,14 @@ var velVec = Vector2();
 
 # Called to pass creator ID information
 func init(parentNumber, rot):
-	parentNumber = parentNumber;
+	self.parentNumber = parentNumber;
 	velVec.x = cos(rot)
 	velVec.y = sin(rot)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.x += velVec.x;
-	self.y += velVec.y;
+	position.x += velVec.x;
+	position.y += velVec.y;
 
 # Called when the cannonball enters a body
 func _on_Cannonball_body_entered(body):
@@ -22,4 +22,4 @@ func _on_Cannonball_body_entered(body):
 	# If we've entered a player, ensure it's not the player that made us and then do damage
 	if (body in players && body.playerNumber != parentNumber):
 		body.health -= 1
-	queue_free()
+		queue_free()
