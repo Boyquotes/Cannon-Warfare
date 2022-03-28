51dd7cf997484eaa2c371934e6183f5eda1742be
Nathan <lassinganathan@gmail.com>
2022-03-28 15:01:33 -0700

    rename all constants to match typical convention

--- objects/PlayerShip.gd
+++ objects/PlayerShip.gd
@@ -4,29 +4,31 @@ export (PackedScene) var Cannonball
 
 ## Constants
 # Movement constants
-export (float) var rotDampConstant; # Dampening constant of the rotation
-export (float) var rotAcc; # Angular Acceleration
-export (float) var velDampConstant; # Dampening constant of the linear velocity
-export (int) var maxRotSpeed; # Maximum rotation speed
-export (int) var accRateOfChange; # Rate at which linear acceleration increases
-export (int) var maxSpeed; # Maximum linear speed
-export (float) var knockbackMultiplier; # Multiplier for the knockback vector. Must be greater than 1 to offset the velocity before the collision
-export (float) var defaultKnockback; # Smallest amount of knockback possible
+export (float) var ROT_DAMP_CONSTANT; # Dampening constant of the rotation
+export (float) var ROT_ACC; # Angular Acceleration
+export (float) var VEL_DAMP_CONSTANT; # Dampening constant of the linear velocity
+export (int) var MAX_ROT_SPEED; # Maximum rotation speed
+export (int) var ACC_RATE_OF_CHANGE; # Rate at which linear acceleration increases
+export (int) var MAX_SPEED; # Maximum linear speed
+export (float) var KNOCKBACK_MULTIPLIER; # Multiplier for the knockback vector. Must be greater than 1 to offset the velocity before the collision
+export (float) var DEFAULT_KNOCKBACK; # Smallest amount of knockback possible
 
 # Other constants
-export (int) var health; # HP of the player, number set in inspector is starting health
-export (int) var playerNumber; # Player ID
-export (int) var damageFromCollision; # Damage taken when colliding with body
+export (int) var PLAYER_NUMBER; # Player ID
+export (int) var DAMAGE_FROM_COLLISION; # Damage taken when colliding with body
+export (int) var CANNON_CHARGE_MAX; # Value at which the cannon is ready to fire
 
 # Declare member variables here.
+export (int) var health; # HP of the player, number set in inspector is starting health
 var velVec = Vector2(); # Velocity Vector
 var accVec = Vector2(); # Acceleration Vector
 var rotVel = 0; # Angular velocity
+var cannonCharge = 0; # How much the cannon is currently charged
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Set sprite based on player id
-	$AnimatedSprite.animation = "player" + str(playerNumber) + "Default";
+	$AnimatedSprite.animation = "player" + str(PLAYER_NUMBER) + "Default";
 	# Add self to player group
 	add_to_group("Players")
 	
@@ -36,24 +38,25 @@ func _physics_process(delta):
 	if (health <= 0):
 		queue_free()
 	
+	# Reset the acceleration vector
 	accVec = Vector2();
 	
 	getInput();
 	
 	# Ensure angular velocity doesn't exceed maximum speed
-	if (abs(rotVel) > maxRotSpeed):
+	if (abs(rotVel) > MAX_ROT_SPEED):
 		if (rotVel < 0):
-			rotVel = maxRotSpeed * -1;
+			rotVel = MAX_ROT_SPEED * -1;
 		else:
-			rotVel = maxRotSpeed;
+			rotVel = MAX_ROT_SPEED;
 			
 	# Update rotation
 	rotation += rotVel * delta;
 	
 	# Update, normalize, and apply velocity
 	velVec += accVec;
-	if (velVec.length() > maxSpeed):
-		velVec = velVec.normalized() * maxSpeed;
+	if (velVec.length() > MAX_SPEED):
+		velVec = velVec.normalized() * MAX_SPEED;
 
 	# Move and handle collisions
 	var collision = move_and_collide(velVec * delta);
@@ -65,17 +68,17 @@ func _physics_process(delta):
 		if (collision.get_collider().has_method("hitByOtherShip")):
 			collision.get_collider().hitByOtherShip(velVec)
 			# Do damage to the ship we hit. We only reach this if our velocity contributed to the collision, otherwise the other ship will go through this instead
-			collision.get_collider().takeDamage(damageFromCollision);
+			collision.get_collider().takeDamage(DAMAGE_FROM_COLLISION);
 			
 		# Do damage to self if collided with something OTHER than a boat
 		else:
-			takeDamage(damageFromCollision)
+			takeDamage(DAMAGE_FROM_COLLISION)
 		
 		# Declare knockback vector. Points from collision to self, multiplied by velocity magnitude and multiplier
-		var knockbackVector = ((self.position - pointOfCollision).normalized() * velVec.length() * knockbackMultiplier);
+		var knockbackVector = ((self.position - pointOfCollision).normalized() * velVec.length() * KNOCKBACK_MULTIPLIER);
 		# If the knockback vector is still zero, then apply the base amount of knockback
 		if (knockbackVector == Vector2(0,0)):
-			knockbackVector = ((self.position - pointOfCollision).normalized() * defaultKnockback)
+			knockbackVector = ((self.position - pointOfCollision).normalized() * DEFAULT_KNOCKBACK)
 		velVec.x += knockbackVector.x
 		velVec.y += knockbackVector.y
 	
@@ -86,30 +89,30 @@ func _physics_process(delta):
 # Handles all input. Called every frame in _physics_process
 func getInput():
 	# Movement
-	if (Input.is_action_pressed("ui_left" + str(playerNumber))):
-		rotVel -= rotAcc;
-	if (Input.is_action_pressed("ui_right" + str(playerNumber))):
-		rotVel += rotAcc;
-	if (Input.is_action_pressed("ui_up" + str(playerNumber))):
+	if (Input.is_action_pressed("ui_left" + str(PLAYER_NUMBER))):
+		rotVel -= ROT_ACC;
+	if (Input.is_action_pressed("ui_right" + str(PLAYER_NUMBER))):
+		rotVel += ROT_ACC;
+	if (Input.is_action_pressed("ui_up" + str(PLAYER_NUMBER))):
 		accVec.x = cos(rotation)
 		accVec.y = sin(rotation)
 		
-		accVec = accVec.normalized() * accRateOfChange;
+		accVec = accVec.normalized() * ACC_RATE_OF_CHANGE;
 		
 	# Shooting
-	if (Input.is_action_just_pressed("ui_shoot" + str(playerNumber))):
+	if (Input.is_action_just_pressed("ui_shoot" + str(PLAYER_NUMBER))):
 		shoot();
 		
 func shoot():
 	# Create first cannonball (aiming left), give position and angle
 	var cannonball0 = Cannonball.instance();
 	cannonball0.position = self.position;
-	cannonball0.init(playerNumber, rotation - PI/2)
+	cannonball0.init(PLAYER_NUMBER, rotation - PI/2)
 
 	# Create second cannonball (aiming right), give position and angle
 	var cannonball1 = Cannonball.instance();
 	cannonball1.position = self.position;
-	cannonball1.init(playerNumber, rotation + PI/2)
+	cannonball1.init(PLAYER_NUMBER, rotation + PI/2)
 	
 	# Add as a child to the cannonballHolder child node. If we just add it as a child of the Player node, then it'll move weird
 	# when the Player node moves due to how child transform is handled
@@ -129,27 +132,27 @@ func takeDamage(damageAmount):
 	
 # Dampens the angular velocity. Called every frame in _physics_process
 func dampenRotation():
-	# If making rotVel approach zero by the rotDampConstant would make it switch signs, then just set it to zero
-	if (abs(rotVel - rotDampConstant) < rotDampConstant):
+	# If making rotVel approach zero by the ROT_DAMP_CONSTANT would make it switch signs, then just set it to zero
+	if (abs(rotVel - ROT_DAMP_CONSTANT) < ROT_DAMP_CONSTANT):
 		rotVel = 0
 		return;
 	else:
-		rotVel -= sign(rotVel) * rotDampConstant;
+		rotVel -= sign(rotVel) * ROT_DAMP_CONSTANT;
 		
 # Dampens the linear velocity. Called when not pressing up
 func dampenVelocity():
 	# Only dampen the velocity if not pressing up. Otherwise, small enough velocities will be completely cancelled out
-	if (Input.is_action_pressed("ui_up" + str(playerNumber))):
+	if (Input.is_action_pressed("ui_up" + str(PLAYER_NUMBER))):
 		return;
 	
-	# If making VelVec's x approach zero by the velDampConstant would make it switch signs, then just set it to zero
-	if (abs(velVec.x - velDampConstant) < velDampConstant):
+	# If making VelVec's x approach zero by the VEL_DAMP_CONSTANT would make it switch signs, then just set it to zero
+	if (abs(velVec.x - VEL_DAMP_CONSTANT) < VEL_DAMP_CONSTANT):
 		velVec.x = 0;
 	else:
-		velVec.x -= sign(velVec.x) * velDampConstant
+		velVec.x -= sign(velVec.x) * VEL_DAMP_CONSTANT
 		
-	# If making VelVec's y approach zero by the velDampConstant would make it switch signs, then just set it to zero
-	if (abs(velVec.y - velDampConstant) < velDampConstant):
+	# If making VelVec's y approach zero by the VEL_DAMP_CONSTANT would make it switch signs, then just set it to zero
+	if (abs(velVec.y - VEL_DAMP_CONSTANT) < VEL_DAMP_CONSTANT):
 		velVec.y = 0;
 	else:
-		velVec.y -= sign(velVec.y) * velDampConstant
+		velVec.y -= sign(velVec.y) * VEL_DAMP_CONSTANT
