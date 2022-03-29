3020c40cd166a6593feb0b1df0ba566ce3ff755d
Nathan <lassinganathan@gmail.com>
2022-03-29 16:39:32 -0700

    fix dependency

---
+++ objects/RockObstacle.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+# This literally will no longer run without this file attached. This wasn't a problem when I comitted to the repository. Why does god forsake me so?
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
